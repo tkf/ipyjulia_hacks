814bc42a87747b3a81766c14bcc351b0e71fdcea
Takafumi Arakaki <aka.tkf@gmail.com>
2018-08-25 00:08:15 -0700

    Patch IPCompleter._completions

--- src/ipyjulia_hacks/completers.py
+++ src/ipyjulia_hacks/completers.py
@@ -1,5 +1,6 @@
 from types import SimpleNamespace
 
+from IPython.core.completer import Completion, IPCompleter
 from cached_property import cached_property
 
 
@@ -10,7 +11,7 @@ class JuliaCompleter:
         self.julia = Julia() if julia is None else julia
 
     @cached_property
-    def jlcomplete(self):
+    def jlcomplete_texts(self):
         return self.julia.eval("""
         import REPL
         (str, pos) -> begin
@@ -19,7 +20,7 @@ class JuliaCompleter:
             if should_complete
                 return map(REPL.completion_text, ret)
             else
-                return String[]
+                return []
             end
         end
         """)
@@ -32,28 +33,43 @@ class JuliaCompleter:
         julia_code = event.line[pos:]
         julia_pos = len(event.text_until_cursor) - pos
 
-        completions = self.jlcomplete(julia_code, julia_pos)
+        completions = self.jlcomplete_texts(julia_code, julia_pos)
         if "." in event.symbol:
             # When completing (say) "Base.s" we need to add the prefix "Base."
             prefix = event.symbol.rsplit(".", 1)[0]
             completions = [".".join((prefix, c)) for c in completions]
         return completions
 
-    def julia_matches(self, ip_completer, text):
-        self.last_text = text
-        if not ip_completer.text_until_cursor.startswith("%%julia"):
+    @cached_property
+    def jlcomplete(self):
+        return self.julia.eval("""
+        import REPL
+        (str, pos) -> begin
+            ret, ran, should_complete = REPL.completions(str, pos)
+            return (
+                map(REPL.completion_text, ret),
+                (first(ran), last(ran)),
+                should_complete,
+            )
+        end
+        """)
+
+    def julia_completions(self, full_text: str, offset: int):
+        self.last_text = full_text
+        if not full_text.startswith("%%julia"):
             return []
         prefix_len = len("%%julia")
-        julia_pos = len(ip_completer.text_until_cursor) - prefix_len
-        julia_code = text[prefix_len:]
-        completions = self.jlcomplete(julia_code, julia_pos)
+        jl_pos = offset - prefix_len
+        jl_code = full_text[prefix_len:]
+        texts, (jl_start, jl_end), should_complete = \
+            self.jlcomplete(jl_code, jl_pos)
+        start = jl_start - 1 + prefix_len
+        end = jl_end + prefix_len
+        completions = [Completion(start, end, txt) for txt in texts]
         self.last_completions = completions
+        # if not should_complete:
+        #     return []
         return completions
-
-    def make_julia_matcher(self, ip_completer):
-        def julia_matches(text):
-            return self.julia_matches(ip_completer, text)
-        return julia_matches
 
     @classmethod
     def instance(cls, *args, **kwargs):
@@ -74,26 +90,24 @@ def patch_ipcompleter():
     if patch_ipcompleter_info is not None:
         return
 
-    from IPython.core.completer import IPCompleter
     patch_ipcompleter_info = _patch_ipcompleter(IPCompleter,
                                                 JuliaCompleter.instance())
 
 
 def _patch_ipcompleter(IPCompleter, jlcompleter):
-    orig_matchers = IPCompleter.matchers
+    orig__completions = IPCompleter._completions
 
-    @property
-    def matchers(self):
-        methods = [
-            jlcompleter.make_julia_matcher(self),
-        ]
-        methods.extend(orig_matchers.__get__(self, IPCompleter))
-        return methods
+    def _completions(self, full_text: str, offset: int, **kwargs):
+        completions = jlcompleter.julia_completions(full_text, offset)
+        if completions:
+            yield from completions
+        else:
+            yield from orig__completions(self, full_text, offset, **kwargs)
 
-    IPCompleter.matchers = matchers
+    IPCompleter._completions = _completions
 
     return SimpleNamespace(
-        orig_matchers=orig_matchers,
-        patched_matchers=matchers,
+        orig__completions=orig__completions,
+        patched__completions=_completions,
         IPCompleter=IPCompleter,
     )
