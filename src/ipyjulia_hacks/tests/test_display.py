17b87fa785a3140b02dabc6e4d526b5b19877cb6
Takafumi Arakaki <aka.tkf@gmail.com>
2018-08-26 15:17:16 -0700

    Add test_mimebundle_pyplot

--- src/ipyjulia_hacks/tests/test_display.py
+++ src/ipyjulia_hacks/tests/test_display.py
@@ -12,3 +12,22 @@ def test_mimebundle_doc(julia):
         "text/markdown",
         "text/latex",
     }
+
+
+def test_mimebundle_pyplot(julia):
+    # Isolate the import step so that error message would be clearer:
+    julia.eval("import PyPlot")
+    obj = julia.eval("""
+    let obj = PyPlot.figure()
+        PyPlot.plot(1:10)
+        obj
+    end
+    """)
+    assert isinstance(obj, JuliaObject)
+    format_dict = obj._repr_mimebundle_()
+    mimes = set(format_dict)
+    assert mimes >= {
+        "text/plain",
+        "application/pdf",
+        "image/png",
+    }
