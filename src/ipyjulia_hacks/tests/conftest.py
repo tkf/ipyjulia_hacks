2c7ac4ac03635775535a081ac0c6502bec2cb959
Takafumi Arakaki <aka.tkf@gmail.com>
2018-08-26 09:16:37 -0700

    Add test_wrappers.py

--- src/ipyjulia_hacks/tests/conftest.py
+++ src/ipyjulia_hacks/tests/conftest.py
@@ -11,3 +11,9 @@ def julia():
         pytest.skip("JuliaAPI is not initialized.")
     return julia
 # JuliaAPI has to be initialized elsewhere (e.g., in top-level conftest.py)
+
+
+@pytest.fixture
+def Main(julia):
+    """ pytest fixture for providing a Julia `Main` name space. """
+    return julia.Main
---
+++ src/ipyjulia_hacks/tests/test_wrappers.py
@@ -0,0 +1,9 @@
+def test_peal(Main):
+    xs = Main.map(Main.identity, range(3))
+    assert all(xs == [0, 1, 2])
+
+
+def test_wrapped_dict(Main, julia):
+    dct = julia.eval("dct = Dict()", force_jlwrap=True)
+    dct["key"] = "value"
+    assert Main.dct == {"key": "value"}
