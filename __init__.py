0e139186f1ecdddffb2730b49b2b6260668c3cfb
Takafumi Arakaki <aka.tkf@gmail.com>
2018-08-29 21:46:49 -0700

    Add JuliaMain

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
-from .initializer import initialize_api, get_api, revise
+from .initializer import initialize_api, get_api, \
+    initialize_main, get_main, \
+    revise
 from .wrappers import jlfunction
 from .julia_api import JuliaAPI, banner
