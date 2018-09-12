92f25cdee27d75570d8a38db514d5e430233fd0f
Takafumi Arakaki <aka.tkf@gmail.com>
2018-09-12 01:45:42 -0700

    Ping Julia via asyncio

--- src/ipyjulia_hacks/ipy/magic.py
+++ src/ipyjulia_hacks/ipy/magic.py
@@ -1,4 +1,8 @@
+import asyncio
+import warnings
+
 from julia import magic
+import IPython
 
 from ..core import get_api, banner
 
@@ -13,8 +17,24 @@ class JuliaMagicsEnhanced(magic.JuliaMagics):
         banner(self._julia)
 
 
+async def julia_eventloop(julia):
+    while True:
+        julia.eval("sleep(0)")
+        await asyncio.sleep(0.05)
+
+
+def start_julia_eventloop():
+    if int(IPython.__version__.split(".", 1)[0]) < 7:
+        warnings.warn("Julia event loop is not running in IPython < v7")
+        return
+
+    asyncio.ensure_future(julia_eventloop(get_api()))
+
+
 def load_ipython_extension(ip):
     ip.register_magics(JuliaMagicsEnhanced)
+
+    start_julia_eventloop()
 
     from . import completers
     completers.patch_ipcompleter()  # monkey patch to make cell magics work
