d2b98c2a82de9a27e9146c3622701a73cf7cd42d
Takafumi Arakaki <aka.tkf@gmail.com>
2018-09-16 20:58:53 -0700

    Capture Julia stdio during test

--- src/ipyjulia_hacks/core/conftest.py
+++ src/ipyjulia_hacks/core/conftest.py
@@ -4,11 +4,13 @@ from . import get_cached_api
 
 
 @pytest.fixture
-def julia():
+def julia(request):
     """ pytest fixture for providing a `JuliaAPI` instance. """
     julia = get_cached_api()
     if julia is None:
         pytest.skip("JuliaAPI is not initialized.")
+    if request.config.getoption("capture") != "no":
+        julia.connect_stdio()
     return julia
 # JuliaAPI has to be initialized elsewhere (e.g., in top-level conftest.py)
 
