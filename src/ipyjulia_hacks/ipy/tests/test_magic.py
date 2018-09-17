3ca83a208c715fd54df5d4f3f52a7184c9147deb
Takafumi Arakaki <aka.tkf@gmail.com>
2018-09-16 21:16:41 -0700

    Fix test_magic.py

--- src/ipyjulia_hacks/ipy/tests/test_magic.py
+++ src/ipyjulia_hacks/ipy/tests/test_magic.py
@@ -4,36 +4,40 @@ from ...core.wrappers import JuliaObject
 
 
 def test_line_magic_simple(ipy_with_magic):
-    ans = ipy_with_magic.run_cell(dedent("""
+    result = ipy_with_magic.run_cell(dedent("""
     %julia :symbol
     """))
+    ans = result.result
     assert isinstance(ans, JuliaObject)
     assert str(ans) == "symbol"
 
 
 def test_cell_magic_simple(ipy_with_magic):
-    ans = ipy_with_magic.run_cell(dedent("""
+    result = ipy_with_magic.run_cell(dedent("""\
     %%julia
     :symbol
     """))
+    ans = result.result
     assert isinstance(ans, JuliaObject)
     assert str(ans) == "symbol"
 
 
-def test_line_magic_help(ipy_with_magic, Main):
-    ans = ipy_with_magic.run_cell(dedent("""
+def test_line_magic_help(ipy_with_magic, julia):
+    result = ipy_with_magic.run_cell(dedent("""
     %julia ?sin
     """))
+    ans = result.result
     assert isinstance(ans, JuliaObject)
-    MD = Main.eval("import Markdown; Markdown.MD")
-    assert Main.isa(ans, MD)
+    MD = julia.eval("import Markdown; Markdown.MD")
+    assert julia.isa(ans, MD)
 
 
-def test_cell_magic_help(ipy_with_magic, Main):
-    ans = ipy_with_magic.run_cell(dedent("""
+def test_cell_magic_help(ipy_with_magic, julia):
+    result = ipy_with_magic.run_cell(dedent("""\
     %%julia
     ?sin
     """))
+    ans = result.result
     assert isinstance(ans, JuliaObject)
-    MD = Main.eval("import Markdown; Markdown.MD")
-    assert Main.isa(ans, MD)
+    MD = julia.eval("import Markdown; Markdown.MD")
+    assert julia.isa(ans, MD)
